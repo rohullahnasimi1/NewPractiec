74f5f31bcd5d76ba328c9c220c9a09efe23d1f0b
Rohullah <rohullah.teksoa@gmail.com>
2020-07-17 13:13:57 -0400

    Changes were made newly!

---
+++ PracticeRepoProject/src/main/java/com/demo/tek/PracticeRepoProject/HelloGitHub.java
@@ -0,0 +1,10 @@
+package com.demo.tek.PracticeRepoProject;
+
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GitHUb");
+	}
+
+}
